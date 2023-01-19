f2486228d3574c427629b87e18fbfd85f3f18258
muchaisam <samson.muchai@strathmore.edu>
2023-01-19 23:03:17 +0300

    feat : main app js

---
+++ public/assets/js/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "......................",
+    authDomain: "......................",
+    databaseURL: "......................",
+    projectId: "......................",
+    storageBucket: ".......................appspot.com",
+    messagingSenderId: "......................",
+    appId: "1:......................:web:4a4470fed53f34f7ed82de",
+    measurementId: "......................"
+  };
